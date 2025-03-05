a998dc1139c9569f4974d914a4ec64bebdc88302
Shivasai Mamidala <105928178+shivasai789@users.noreply.github.com>
2025-03-05 19:18:38 +0530

    modified tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import animate from 'tailwindcss-animate';
 export default {
     darkMode: ["class"],
     content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
@@ -53,6 +54,6 @@ export default {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [animate],
 }
 
